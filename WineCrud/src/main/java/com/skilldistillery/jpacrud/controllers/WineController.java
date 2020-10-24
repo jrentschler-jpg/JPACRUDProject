936e7011024cc9313bd35ee2658c102845b16ff9
Jourdan Rentschler <jrentschler3@gmail.com>
2020-10-23 18:56:57 -0600

    Added Boot with controllers and index.jsp

---
+++ WineCrud/src/main/java/com/skilldistillery/jpacrud/controllers/WineController.java
@@ -0,0 +1,20 @@
+package com.skilldistillery.jpacrud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class WineController {
+	
+	
+	
+	@RequestMapping(path = "/")
+	public String index() {
+		return "index";
+	}
+	
+	
+
+}
