b2c232d29ca837defcc8eeb9edb47b2f5e937ab8
Jourdan Rentschler <jrentschler3@gmail.com>
2020-10-23 19:56:24 -0600

    Created the DAO

---
+++ WineCrud/src/main/java/com/skilldistillery/jpacrud/data/WineDAO.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.jpacrud.data;
+
+public interface WineDAO {
+
+}
---
+++ WineCrud/src/main/java/com/skilldistillery/jpacrud/data/WineDAOJpaImpl.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.jpacrud.data;
+
+public class WineDAOJpaImpl implements WineDAO {
+
+}
