50256a73080ee25094660dff02d159a2079b4fcc
Jourdan Rentschler <jrentschler3@gmail.com>
2020-10-26 10:13:18 -0600

    WAR deployment configuration.

--- WineCrud/src/main/java/com/skilldistillery/jpacrud/WineCrudApplication.java
+++ WineCrud/src/main/java/com/skilldistillery/jpacrud/WineCrudApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.jpacrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WineCrudApplication {
+public class WineCrudApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WineCrudApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WineCrudApplication.class, args);
